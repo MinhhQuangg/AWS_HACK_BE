d59ee22cc341647944595a8fbc58ef6c087126a9
linhphan03 <phanthuylinh221203@gmail.com>
2025-05-21 13:56:57 -0700

    init project:

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/users", require("./routes/userRoutes"));
+// app.use("/api/sessions", require("./routes/sessionRoutes"));
+// app.use("/api/messages", require("./routes/messageRoutes"));
+// app.use("/api/feedback", require("./routes/feedbackRoutes"));
+// app.use("/api/scenarios", require("./routes/scenarioRoutes"));
+
 module.exports = app;
